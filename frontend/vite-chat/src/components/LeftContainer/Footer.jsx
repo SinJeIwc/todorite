34ba95cbda1bd2ed0d99b573e87c9fa602ad90f9
jarnovs <usalmazovich@gmail.com>
2025-03-21 21:07:49 +0600

    Added sign out

--- frontend/vite-chat/src/components/LeftContainer/Footer.jsx
+++ frontend/vite-chat/src/components/LeftContainer/Footer.jsx
@@ -2,14 +2,17 @@ import sign_out_img from "../../../public/img/sign_out.png";
 import useUser from "../../services/useUser.jsx";
 
 function Footer({me}) {
-
+  const handleSignOut = () => {
+    document.cookie = "token=; path=/;";
+    window.location.href = "/login";
+  };
   return (
     <footer className="profile_container">
       <div className="profile_btn">
         <img id="profile_logo" src={me.profile_logo} alt="profile" />
         <span className="nickname">{me.name}</span>
       </div>
-      <button className="sign_out_btn js-sign_out_btn">
+      <button onClick={handleSignOut} className="sign_out_btn js-sign_out_btn">
         <img id="sign_out_logo" src={sign_out_img} alt="sign out" />
       </button>
     </footer>
